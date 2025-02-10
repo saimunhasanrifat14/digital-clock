bcd00183e20ad7ee29b26088f4c83742d4e88731
Saimun Hasan <saimunhasanrifat14@gmail.com>
2025-02-10 22:45:02 +0600

    add functionality for create a digital clock

--- src/Component/DigitalClock.jsx
+++ src/Component/DigitalClock.jsx
@@ -1,7 +1,23 @@
 import { useState, useEffect } from "react";
 
 const DigitalClock = () => {
-  
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval); 
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center bg-red-500 w-full">
+      <div className="text-center">
+        <h1>{time.toLocaleTimeString()}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default DigitalClock;
